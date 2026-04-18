352c5a3f5528fee79b36b54c7a888cf8fe60b00c
Jonathan Tobon <jtobonc@unicartagena.edu.co>
2026-04-17 22:03:46 -0500

    feat: agregar entidades de persistencia para User y EntradaCine

---
+++ Infrastructure/Adapters/Persistence/MySQL/Entity/EntradaCineEntity.php
@@ -0,0 +1,133 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Infrastructure\Adapters\Persistence\MySQL\Entity;
+
+final class EntradaCineEntity
+{
+    private string  $id;
+    private string  $fechaCompra;
+    private string  $fechaEntrada;
+    private string  $horaInicio;
+    private string  $horaFin;
+    private string  $valor;
+    private string  $pelicula;
+    private string  $puesto;
+    private string  $sala;
+    private string  $genero;
+    private string  $cine;
+    private string  $pais;
+    private string  $departamento;
+    private string  $ciudad;
+    private string  $centroComercial;
+    private ?string $createdAt;
+    private ?string $updatedAt;
+
+    public function __construct(
+        string  $id,
+        string  $fechaCompra,
+        string  $fechaEntrada,
+        string  $horaInicio,
+        string  $horaFin,
+        string  $valor,
+        string  $pelicula,
+        string  $puesto,
+        string  $sala,
+        string  $genero,
+        string  $cine,
+        string  $pais,
+        string  $departamento,
+        string  $ciudad,
+        string  $centroComercial,
+        ?string $createdAt = null,
+        ?string $updatedAt = null
+    ) {
+        $this->id              = trim($id);
+        $this->fechaCompra     = trim($fechaCompra);
+        $this->fechaEntrada    = trim($fechaEntrada);
+        $this->horaInicio      = trim($horaInicio);
+        $this->horaFin         = trim($horaFin);
+        $this->valor           = trim($valor);
+        $this->pelicula        = trim($pelicula);
+        $this->puesto          = trim($puesto);
+        $this->sala            = trim($sala);
+        $this->genero          = trim($genero);
+        $this->cine            = trim($cine);
+        $this->pais            = trim($pais);
+        $this->departamento    = trim($departamento);
+        $this->ciudad          = trim($ciudad);
+        $this->centroComercial = trim($centroComercial);
+        $this->createdAt       = $createdAt;
+        $this->updatedAt       = $updatedAt;
+    }
+
+    public function id(): string
+    {
+        return $this->id;
+    }
+    public function fechaCompra(): string
+    {
+        return $this->fechaCompra;
+    }
+    public function fechaEntrada(): string
+    {
+        return $this->fechaEntrada;
+    }
+    public function horaInicio(): string
+    {
+        return $this->horaInicio;
+    }
+    public function horaFin(): string
+    {
+        return $this->horaFin;
+    }
+    public function valor(): string
+    {
+        return $this->valor;
+    }
+    public function pelicula(): string
+    {
+        return $this->pelicula;
+    }
+    public function puesto(): string
+    {
+        return $this->puesto;
+    }
+    public function sala(): string
+    {
+        return $this->sala;
+    }
+    public function genero(): string
+    {
+        return $this->genero;
+    }
+    public function cine(): string
+    {
+        return $this->cine;
+    }
+    public function pais(): string
+    {
+        return $this->pais;
+    }
+    public function departamento(): string
+    {
+        return $this->departamento;
+    }
+    public function ciudad(): string
+    {
+        return $this->ciudad;
+    }
+    public function centroComercial(): string
+    {
+        return $this->centroComercial;
+    }
+    public function createdAt(): ?string
+    {
+        return $this->createdAt;
+    }
+    public function updatedAt(): ?string
+    {
+        return $this->updatedAt;
+    }
+}
